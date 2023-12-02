29b98868f943b404af76760dd8e232e8cb8256cb
Antoniongola <Antoniongola10@gmail.com>
2023-12-02 14:17:03 +0100

    adding the services of our product

---
+++ src/main/java/com/ngola/ecommerce/productcatalog/services/ProductService.java
@@ -0,0 +1,53 @@
+package com.ngola.ecommerce.productcatalog.services;
+
+import com.ngola.ecommerce.productcatalog.entities.Product;
+import com.ngola.ecommerce.productcatalog.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService implements ProductServiceInterface{
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product getProductById(long id){
+        if(this.productRepository.findById(id).isPresent()){
+            return this.productRepository.findById(id).get();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public boolean addProduct(Product product) {
+        if(product != null){
+            this.productRepository.save(product);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateProduct(long id, Product product) {
+        if(product != null){
+            this.productRepository.save(product);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteProduct(long id) {
+        if(this.productRepository.findById(id).isPresent()){
+            this.productRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/ngola/ecommerce/productcatalog/services/ProductServiceInterface.java
@@ -0,0 +1,14 @@
+package com.ngola.ecommerce.productcatalog.services;
+
+import com.ngola.ecommerce.productcatalog.entities.Product;
+
+import java.util.List;
+
+public interface ProductServiceInterface {
+    public Product getProductById(long id);
+    public List<Product> getAllProducts();
+    public boolean addProduct(Product product);
+
+    public boolean updateProduct(long id, Product product);
+    public boolean deleteProduct(long id);
+}
